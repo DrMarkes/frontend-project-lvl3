0a7794bf700f0a0d253398fa9329526734dc5e1b
DrMarkes <markovandrew@yandex.ru>
2022-03-20 16:41:17 +0700

    fix 4

--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import 'bootstrap';
 import i18next from 'i18next';
 import ru from './locales/ru.js';
 import app from './app.js';
@@ -5,11 +6,13 @@ import app from './app.js';
 export default () => {
   const i18nextInstance = i18next.createInstance();
   const lang = window.navigator.userLanguage || window.navigator.language;
-  i18nextInstance.init({
-    lng: lang,
-    debug: false,
-    resources: {
-      ru,
-    },
-  }).then(() => app(i18nextInstance));
+  i18nextInstance
+    .init({
+      lng: lang,
+      debug: false,
+      resources: {
+        ru,
+      },
+    })
+    .then(() => app(i18nextInstance));
 };
